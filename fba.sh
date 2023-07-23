2fdc64584d6cfb2abfb7de6bea51a6ac1a5d9635
Doug Wegscheid <dwegscheid@sbcglobal.net>
2023-07-23 12:26:15 -0400

    Hang on to some more files.

---
+++ fba.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+doit() {
+	./do.sh $1
+}
+
+doit ../FBA/ASTERIX.txt
+doit ../FBA/BETHOVEN.txt
+doit ../FBA/BRIDGET.txt
+doit ../FBA/CAT1.txt
+doit ../FBA/CAT2.txt
+doit ../FBA/CLIMBER.txt
+doit ../FBA/DEAN.txt
+doit ../FBA/DUCK.txt
+doit ../FBA/EINSTEIN.txt
+doit ../FBA/GGBRIDGE.txt
+doit ../FBA/GORILLA.txt
+doit ../FBA/HORSE.txt
+doit ../FBA/LUDWIG.txt
+doit ../FBA/MARILYN.txt
+doit ../FBA/MINIMONA.txt
+doit ../FBA/MONALISA.txt
+doit ../FBA/MOON.txt
+doit ../FBA/NIGHT.txt
+doit ../FBA/NUDE1.txt
+doit ../FBA/NUDE2.txt
+doit ../FBA/NUDE3.txt
+doit ../FBA/NUDE.txt
+doit ../FBA/OGLEAP.txt
+doit ../FBA/SPOCK.txt
+doit ../FBA/SYLVETTE.txt
